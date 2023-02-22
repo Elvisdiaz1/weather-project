2c4330599a36c4e0947dfc68aac1300c9b68d42f
Elvis Diaz <elvisdiaz020@gmail.com>
2023-02-21 22:25:07 -0500

    adds a country code input and future 5 day weather functionality

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let currentWeatherEl = document.getElementById("current-weather");
 let futureWeatherEl = document.getElementById("future-weather");
-let inputEl = document.getElementById("input");
+let inputCityEl = document.getElementById("input-city");
+let inputCountryEl = document.getElementById("input-country");
 let button = document.getElementById("search-button");
 let weatherList = document.getElementById("weather-list");
 
@@ -10,7 +11,8 @@ let futureWeather = document.createElement("p");
 currentWeatherEl.appendChild(currentWeather);
 futureWeatherEl.appendChild(futureWeather);
 
-let value = inputEl.value;
+let Cityvalue = inputCityEl.value;
+let Countryvalue = inputCountryEl.value;
 
 function getApi(lat, lon) {
   let api = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=b8228c83264e4447e4bd0157e1a7f01f`;
@@ -21,32 +23,29 @@ function getApi(lat, lon) {
     .then(function (data) {
       console.log(data);
       currentWeather.textContent = data.list[0].weather[0].description;
-      for (let index = 0; index < data.list.length; index++) {
+      for (let index = 1; index < 6; index++) {
         let listItems = document.createElement("li");
+        weatherList.appendChild(listItems);
+        listItems.textContent = data.list[index].weather[0].description;
       }
     });
 }
 
-function getLocation() {
-  let cityLocator = `http://api.openweathermap.org/geo/1.0/direct?q=brooklyn&limit=5&appid=b8228c83264e4447e4bd0157e1a7f01f`;
+function getLocation(city, country) {
+  let cityLocator = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${country}&limit=5&appid=b8228c83264e4447e4bd0157e1a7f01f`;
   fetch(cityLocator)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data);
-      //   console.log(data[0].lat);
-      //   console.log(data[0].lon);
       let lat = data[0].lat;
       let lon = data[0].lon;
       getApi(lat, lon);
     });
 }
 
-getLocation(inputEl.value);
-
 button.addEventListener("click", clickButton);
 function clickButton() {
-  console.log(inputEl.value);
-  console.log("1");
+  getLocation(inputCityEl.value, inputCountryEl.value);
 }
