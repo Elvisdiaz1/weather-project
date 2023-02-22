82aeaf919898a3981555857afdcc7dca5f02e405
Elvis Diaz <elvisdiaz020@gmail.com>
2023-02-21 23:09:05 -0500

    adds temp calc from kelvin to celsius and adds humidity and wind to weather showing

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ let futureWeatherEl = document.getElementById("future-weather");
 let inputCityEl = document.getElementById("input-city");
 let inputCountryEl = document.getElementById("input-country");
 let button = document.getElementById("search-button");
-let weatherList = document.getElementById("weather-list");
+let futureWeatherList = document.getElementById("weather-list");
+let currentWeatherList = document.getElementById("current-weather-list");
 
 let currentWeather = document.createElement("p");
 let futureWeather = document.createElement("p");
@@ -22,11 +23,33 @@ function getApi(lat, lon) {
     })
     .then(function (data) {
       console.log(data);
-      currentWeather.textContent = data.list[0].weather[0].description;
+      let listItems = document.createElement("li");
+      let temperatureItem = document.createElement("p");
+      let humidityItem = document.createElement("p");
+      let windItem = document.createElement("p");
+      currentWeatherList.appendChild(listItems);
+      listItems.appendChild(temperatureItem);
+      listItems.appendChild(humidityItem);
+      listItems.appendChild(windItem);
+      temperatureItem.textContent =
+        "Temp: " + convertKtoF(data.list[0].main.temp) + " F";
+      humidityItem.textContent =
+        "Humidity: " + data.list[0].main.humidity + " %";
+      windItem.textContent = "Wind: " + data.list[0].wind.speed + " MPH";
       for (let index = 1; index < 6; index++) {
         let listItems = document.createElement("li");
-        weatherList.appendChild(listItems);
-        listItems.textContent = data.list[index].weather[0].description;
+        let temperatureItem = document.createElement("p");
+        let humidityItem = document.createElement("p");
+        let windItem = document.createElement("p");
+        futureWeatherList.appendChild(listItems);
+        listItems.appendChild(temperatureItem);
+        listItems.appendChild(humidityItem);
+        listItems.appendChild(windItem);
+        temperatureItem.textContent =
+          "Temp: " + convertKtoF(data.list[index].main.temp) + " F";
+        humidityItem.textContent =
+          "Humidity: " + data.list[index].main.humidity + " %";
+        windItem.textContent = "Wind: " + data.list[index].wind.speed + " MPH";
       }
     });
 }
@@ -48,4 +71,9 @@ function getLocation(city, country) {
 button.addEventListener("click", clickButton);
 function clickButton() {
   getLocation(inputCityEl.value, inputCountryEl.value);
+}
+
+function convertKtoF(temp) {
+  let result = Math.floor((temp - 273.15) * (9 / 5) + 32);
+  return result;
 }
